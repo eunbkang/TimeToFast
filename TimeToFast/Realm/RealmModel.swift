fa59bb6769827077f7ee399846e357b981ddf1ea
Rain <eunbkang@gmail.com>
2023-10-12 05:04:15 +0900

    [feat] added Realm package and created FastingRecord model

---
+++ TimeToFast/Realm/RealmModel.swift
@@ -0,0 +1,34 @@
+//
+//  RealmModel.swift
+//  TimeToFast
+//
+//  Created by Eunbee Kang on 2023/10/12.
+//
+
+import Foundation
+import RealmSwift
+
+class FastingRecord: Object {
+    @Persisted(primaryKey: true) var _id: ObjectId
+    @Persisted var date: Date
+    @Persisted var fastingPlan: String
+    @Persisted var fastingStartTime: Date
+    @Persisted var fastingEndTime: Date
+    @Persisted var note: String
+    @Persisted var fastingDuration: Int
+    @Persisted var eatingDuration: Int
+    @Persisted var isGoalAchieved: Bool
+    
+    convenience init(date: Date, fastingPlan: String, fastingStartTime: Date, fastingEndTime: Date, note: String, fastingDuration: Int, eatingDuration: Int, isGoalAchieved: Bool) {
+        self.init()
+        
+        self.date = date
+        self.fastingPlan = fastingPlan
+        self.fastingStartTime = fastingStartTime
+        self.fastingEndTime = fastingEndTime
+        self.note = note
+        self.fastingDuration = fastingDuration
+        self.eatingDuration = eatingDuration
+        self.isGoalAchieved = isGoalAchieved
+    }
+}
