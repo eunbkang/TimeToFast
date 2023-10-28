9981048b55823501c5f2ee9ef0dd7b3beba9859c
Rain <eunbkang@gmail.com>
2023-10-28 20:19:35 +0900

    [Feat] gradient background

--- TimeToFastWidget/WidgetView/TimerWidgetView.swift
+++ TimeToFastWidget/WidgetView/TimerWidgetView.swift
@@ -24,50 +24,55 @@ struct TimerWidgetView: View {
     
     var body: some View {
         ZStack {
+            Color(.white)
+            LinearGradient(colors: [Color("lightPurple"), Color("lightGreen")], startPoint: .topLeading, endPoint: UnitPoint(x: 1, y: 1.5))
+            
             ZStack {
-                Circle()
-                    .trim(from: 0.0, to: 0.7)
-                    .stroke(style: StrokeStyle(lineWidth: 9, lineCap: .butt))
-                    .foregroundColor(Color("lightPurple"))
-                    .rotationEffect(Angle(degrees: -155.0))
-                
-                Circle()
-                    .trim(from: 0.0, to: 0.32)
-                    .stroke(style: StrokeStyle(lineWidth: 9, lineCap: .butt))
-                    .foregroundColor(Color("lightGreen"))
-                    .rotationEffect(Angle(degrees: 90))
-            }
-            
-            VStack {
-                Image(systemName: "moon.stars.fill")
-                    .foregroundColor(.blue)
-                    .font(.caption2)
-                
-                Spacer()
-                
-                Image(systemName: "sun.max.fill")
-                    .foregroundColor(.orange)
-                    .font(.caption2)
-            }
-            .padding(9)
-            
-            VStack(spacing: 4) {
-                Text(data.stateTitle)
-                    .font(.footnote)
-                    .fontWeight(.semibold)
+                ZStack {
+                    Circle()
+                        .trim(from: 0.0, to: 0.7)
+                        .stroke(style: StrokeStyle(lineWidth: 9, lineCap: .butt))
+                        .foregroundColor(Color("lightPurple"))
+                        .rotationEffect(Angle(degrees: -155.0))
+                    
+                    Circle()
+                        .trim(from: 0.0, to: 0.32)
+                        .stroke(style: StrokeStyle(lineWidth: 9, lineCap: .butt))
+                        .foregroundColor(Color("lightGreen"))
+                        .rotationEffect(Angle(degrees: 90))
+                }
                 
                 VStack {
-                    Text(data.timeCounter)
-                        .font(.title3)
-                        .fontWeight(.heavy)
-                    
-                    Text("REMAINING")
+                    Image(systemName: "moon.stars.fill")
+                        .foregroundColor(.blue)
                         .font(.caption2)
-                        .foregroundColor(.gray)
+                    
+                    Spacer()
+                    
+                    Image(systemName: "sun.max.fill")
+                        .foregroundColor(.orange)
+                        .font(.caption2)
+                }
+                .padding(9)
+                
+                VStack(spacing: 4) {
+                    Text(data.stateTitle)
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                    
+                    VStack {
+                        Text(data.timeCounter)
+                            .font(.title3)
+                            .fontWeight(.heavy)
+                        
+                        Text("REMAINING")
+                            .font(.caption2)
+                            .foregroundColor(.gray)
+                    }
                 }
             }
+            .padding(16)
         }
-        .padding(16)
     }
 }
 
